6ee0d5c1489573a05f25751b000ff7a241370342
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2020-01-15 16:13:35 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,30 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.dirname(__file__), 'ievv_auth', 'version.json')) as f:
     version = json.loads(f.read())
 
+
 setup(
     name='ievv-auth',
-    description='The ievv_auth django project.',
+    description='Authentication modules for the Django framework.',
     version=version,
-    author='ievv',
+    author='Appresso developers',
+    author_email='post@appresso.no',
     packages=find_packages(exclude=['manage']),
+    license='BSD',
     install_requires=[
-        'Django>=1.11',
-        'PyJWT>=1.7.1'
+        'Django',
+        'PyJWT>=1.7.1',
+        'djangorestframework',
+        'psycopg2'
     ],
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
 )
